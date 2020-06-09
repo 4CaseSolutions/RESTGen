d49c3a2601e90a507934126e6bcf93a699184938
Neto Silva <alnetosilva@gmail.com>
2020-06-08 23:37:24 -0300

    inciando nosso projeto e documentando a segunda aula

---
+++ src/app.js
@@ -0,0 +1,7 @@
+// Isto é um comentário
+// Abaixo vamos definir uma constante chamada a
+const a = "Hello ";
+// Abaixo vamos definir uma constante chamada b
+const b = "World";
+// Abaixo vamos mandar imprimir no console, o conteúdo das nossas constantes
+console.log( a + b );
